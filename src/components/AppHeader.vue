9392db897968bd9209a94b2dd84fe23c23ae9a40
Andriy Dyachyk <andriy.dyachyk98@gmail.com>
2023-06-27 12:08:18 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  
+}
+</script>
+<template lang="">
+  <div>
+    hello world
+  </div>
+</template>
+<style lang="">
+  
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  
+}
+</script>
+<template lang="">
+  <div>
+    I'm main
+  </div>
+</template>
+<style lang="">
+  
+</style>
